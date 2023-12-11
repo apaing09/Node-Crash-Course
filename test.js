d5c66ab9f73da439b9777ddf743724bc92c5cb90
Aung Zin Paing <135940668+apaing09@users.noreply.github.com>
2023-12-10 22:32:03 -0500

    Add files via upload

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
-const name = 'Yoshi';
+// const name = 'Yoshi';
 
-console.log(name);
+// console.log(name);
+
+const greet = (name) => {
+    console.log(`hello, ${name}`);
+}
+
+greet('mario');
+greet('yoshi');
